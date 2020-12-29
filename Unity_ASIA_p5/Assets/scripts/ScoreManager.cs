05a56a73e76f8a61554cc1fb2293fb9415ec6d6d
b50905du <71679904+b50905du@users.noreply.github.com>
2020-12-29 11:49:10 +0800

    分數得分器

--- Unity_ASIA_p5/Assets/scripts/ScoreManager.cs
+++ Unity_ASIA_p5/Assets/scripts/ScoreManager.cs
@@ -7,9 +7,41 @@ public class ScoreManager : MonoBehaviour
     public Text text;
     [Header("分數")]
     public int score;
+    [Header("得分")]
+    public int scoreIn=2;
+    [Header("進球音效")]
+    public AudioClip soundIn;
+
+    private AudioSource aud;
+
+    private void Awake()
+    {
+        aud = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        print(other.name);
+        if (other.tag=="籃球")
+        {
+            AddScore();
+        }
+        if (other.transform.root.name=="Player")
+        {
+            scoreIn = 3;
+        }
+    }
+    private void OnTriggerExit(Collider other) 
+    {
+        if (other.transform.root.name == "Player")
+        {
+            scoreIn = 2;
+        }
+    }
+
+    private void AddScore()
+    {
+        score += scoreIn;
+        text.text = score.ToString();
+        aud.PlayOneShot(soundIn, Random.Range(1f, 2f));
     }
 }

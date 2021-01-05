1a604f369c7f9a62948b57486362be7ba9b31831
b50905du <71679904+b50905du@users.noreply.github.com>
2021-01-05 11:59:22 +0800

    發布EXE

---
+++ Unity_ASIA_p5/Assets/scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("投籃機");
+    }
+
+    public void QuitGame() 
+    {
+        Application.Quit();
+    }
+}
